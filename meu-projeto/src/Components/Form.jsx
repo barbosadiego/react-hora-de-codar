1bbe6f445a461266d5a5a74965ea3b51e5b59032
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-07 14:24:44 -0300

    hooks

--- meu-projeto/src/Components/Form.jsx
+++ meu-projeto/src/Components/Form.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
+import React from 'react';
 
 const Form = (props) => {
-  function cadastrarUsuario(e){
-    e.preventDefault()
-    console.log('Cadastro efetuado')
+  function cadastrarUsuario(e) {
+    e.preventDefault();
   }
+  const [name, setName] = React.useState(null)
+  const [password, setPassword] =  React.useState(null)
+
   return (
     <div>
       <h1>Meu cadastro:</h1>
       <form onSubmit={cadastrarUsuario}>
         <div>
-          <input type="text" placeholder='Digite o seu nome'/>
+          <label htmlFor="name">Nome: </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={(event) => setName(event.target.value)}
+          />
         </div>
         <div>
-          <input type="submit" value='Cadastrar'/>
+          <label htmlFor="password">Senha: </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Digite sua senha"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Cadastrar" />
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
