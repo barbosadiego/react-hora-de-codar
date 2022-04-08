4a6fc494e0a2767b779ba65b5f9897e67eb234d5
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-08 09:37:44 -0300

    icons

--- meu-projeto/src/Components/layout/Footer.jsx
+++ meu-projeto/src/Components/layout/Footer.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import styles from './Footer.css'
 
 const Footer = (props) => {
   return (
     <footer>
+      <ul className={styles.social_list}>
+        <li>
+          <FaFacebook />
+        </li>
+        <li>
+          <FaTwitter />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+      </ul>
       <p>Todos os direito reservados</p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
