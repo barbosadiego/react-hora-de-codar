07ac649329065d1fb675f0ab2db29e459a799a20
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-08 15:47:27 -0300

    api

--- costs/src/Components/Form/Select.jsx
+++ costs/src/Components/Form/Select.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import styles from './Select.css'
+import React from 'react';
+import styles from './Select.css';
 
-const Select = ({text, name, options, handleOnChange, value}) => {
+const Select = ({ text, name, options, handleOnChange, value }) => {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
       <select name={name} id={name}>
         <option>Selecione uma opção</option>
+        {options.map((item) => (
+          <option value={item.id} key={item.id}>
+            {item.name}
+          </option>
+        ))}
       </select>
     </div>
-  )
-}
+  );
+};
 
-export default Select
+export default Select;
--- costs/src/Components/Project/ProjectForm.jsx
+++ costs/src/Components/Project/ProjectForm.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Input from '../Form/Input';
 import Select from '../Form/Select';
 import SubmitButton from '../Form/SubmitButton';
 import styles from './ProjectForm.css';
 
-const ProjectForm = ({btnText}) => {
+const ProjectForm = ({ btnText }) => {
+  const [categories, setCategories] = React.useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <form className={styles.form}>
       <Input
@@ -20,7 +36,11 @@ const ProjectForm = ({btnText}) => {
         placeholder="Insira o orçamento total"
       />
       <div>
-        <Select name='category-id' text='Selecione uma categoria' />
+        <Select
+          name="category-id"
+          text="Selecione uma categoria"
+          options={categories}
+        />
       </div>
       <SubmitButton text={btnText} />
     </form>
