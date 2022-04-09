4ab004536d64a86f4581daf405950fc41e328343
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-09 10:08:19 -0300

    dashboard

--- costs/src/Components/Pages/Projects.jsx
+++ costs/src/Components/Pages/Projects.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Message from '../Layout/Message';
 import { useLocation } from 'react-router-dom';
+import styles from './Projects.css';
+import Container from '../Layout/Container'
+import LinkButton from '../Layout/LinkButton'
 
 const Projects = (props) => {
   const location = useLocation();
@@ -11,9 +14,15 @@ const Projects = (props) => {
   }
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to='/newproject' text='Criar Projeto' />
+      </div>
       {message && <Message type="success" msg={message} />}
+      <Container customClass='start'>
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 };
